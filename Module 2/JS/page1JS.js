95872e8a83a8d333f9e7607431c51216ad8aa66e
MattFauth <41974587+MattFauth@users.noreply.github.com>
2019-07-01 12:49:58 -0300

    2.4 done

--- Module 2/JS/page1JS.js
+++ Module 2/JS/page1JS.js
@@ -4,6 +4,12 @@ function changeTitle(){
         title.innerHTML += "<br> You changed the <u>title</u> with JavaScript!";
     }
 
+function changeTitleBack(){
+  var title = 
+  document.querySelector("#firstPage");
+    title.innerHTML = "My first page ever"; 
+}
+
 function changeTitleCSS(){
   var title =
   document.querySelector("#firstPage");
@@ -22,54 +28,24 @@ function changeTitleCSSBack(){
   return title;
 }
 
-paperScript = function() {
-  var mousePoint = view.center;
-  var amount = 25;
-  var colors = ['red', 'white', 'blue', 'white'];
-    
-  for (var i = 0; i < amount; i++) {
-    var rect = new Rectangle([0, 0], [25, 25]);
-    rect.center = mousePoint;
-    var path = new Path.Rectangle(rect, 6);
-    path.fillColor = colors[i % 4];
-    var scale = (1 - i / amount) * 20;
-    path.scale(scale);   
+  function changePageBackgroundColor(color){
+    document.body.style.backgroundColor = color;
+    var output = document.querySelector('#chosenColor');
+    output.innerHTML = "The color selected is: " + color;
   }
-    
-  function onMouseMove(event) {
-    mousePoint = event.point;
-  }
-    
-  var children = project.activeLayer.children;
-    
-  function onFrame(event) {
-    for (var i = 0, l = children.length; i < l; i++) {
-      var item = children[i];
-      var delta = (mousePoint - item.position) / (i + 5);
-      item.rotate(Math.sin((event.count + i) / 10) * 7);
-      if (delta.length > 0.1)
-        item.position += delta;
-      }
-    }
-  }
-    
-  var a = document.createElement('script')
-  a.setAttribute('type', 'text/paperscript');
-  a.setAttribute('canvas', 'canvas');
-  var src = paperScript.toString();
-  a.appendChild(document.createTextNode(src.substring(src.indexOf('\n') + 1, src.lastIndexOf('\n'))));
-  document.body.appendChild(a);
 
-  function message1(){
-    var ab = document.querySelector("#change");
-    ab.addEventListener('click', function(evt) {
-      changeTitleCSS();
-      alert("CSS changed");
-    });
-  }
-  function message2(){
-    var ac = document.querySelector('#changeBack');
-    ac.addEventListener('click', function(evt){
-      alert("CSS changed back");
-    });
+  function validateName(field) {
+    var name = field.value;  
+    var output = document.querySelector('#nameTyped');
+    output.innerHTML = "Valid name: " + name;
+    var i = 0
+    while(i < name.length){
+      if (name.charAt(i) === "!" || name.charAt(i) === "?"){
+        output.innerHTML = "This name has invalid characters(! or ?)";
+      }
+      i += 1;
+    }
+    if(name.length < 4) {
+      output.innerHTML = "This name is too short (at least 4 chars)";
+    }
   }
