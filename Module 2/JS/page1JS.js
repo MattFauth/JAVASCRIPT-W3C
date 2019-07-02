57f2ef8bb6ac820d6509160e96069f5895257e34
MattFauth <41974587+MattFauth@users.noreply.github.com>
2019-07-02 11:19:10 -0300

    2.5 done

--- Module 2/JS/page1JS.js
+++ Module 2/JS/page1JS.js
@@ -86,3 +86,18 @@ function changeTitleCSSBack(){
     }){
     }
   }
+
+  function add() {
+    var val = document.querySelector('#newItem').value;
+    if((val !== undefined) && (val !== "")) {
+      var ul = document.querySelector("#items");
+      var newItem = document.createElement("li");
+      newItem.textContent = val;
+      ul.append(newItem);
+    }
+  }
+
+  function reset() {
+    var ul = document.querySelector("#items");
+    ul.innerHTML = ""; 
+  }
